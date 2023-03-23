33d6ad82f6a4aeefd6c3182812c5a5b449c0d74b
Dave Mastropolo <davemastropolo@Margarets-MacBook-Air.local>
2023-03-23 14:59:51 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          14 13 12 11 10 789 test456 test 123 adding more testing Adding text Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
