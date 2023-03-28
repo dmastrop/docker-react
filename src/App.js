9aa3494b53964389255eb2aec48e26c99094d7db
Dave Mastropolo <davemastropolo@Margarets-MacBook-Air.local>
2023-03-28 13:50:21 -0700

    minor changes to App.js in frontend project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          new change to commit to feature 17 16 15 14 13 12 11 10 789 test456 test 123 adding more testing Adding text Edit <code>src/App.js</code> and save to reload.
+         new change to commit to feature 17 16 15 14 13 12 11 10 789 test456 test 123 adding more testing Adding text Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
